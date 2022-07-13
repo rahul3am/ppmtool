4fdb149326d48367a0b3629e7168c51a33bedaad
rahul3am <rahul720p@gmail.com>
2022-07-13 20:07:49 +0530

    Project Table created

--- ppmtool/src/main/java/com/rahul/ppmtool/PpmtoolApplication.java
+++ ppmtool/src/main/java/com/rahul/ppmtool/PpmtoolApplication.java
@@ -1,7 +1,10 @@
 package com.rahul.ppmtool;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class PpmtoolApplication {
