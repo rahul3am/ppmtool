0611b74b6a47043ce63fb9a9bd2a9e14b0e457a4
rahul3am <rahul720p@gmail.com>
2022-07-14 18:58:37 +0530

    Services and Controllers

---
+++ ppmtool/src/main/java/com/rahul/ppmtool/services/ProjectService.java
@@ -0,0 +1,23 @@
+package com.rahul.ppmtool.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rahul.ppmtool.domain.Project;
+import com.rahul.ppmtool.repositories.ProjectRepository;
+
+@Service
+public class ProjectService {
+	
+	@Autowired
+	private ProjectRepository projectRepository;
+	
+	 public Project saveOrUpdateProject(Project project){
+
+	        //Logic
+
+	        return projectRepository.save(project);
+	    }
+
+	
+}
