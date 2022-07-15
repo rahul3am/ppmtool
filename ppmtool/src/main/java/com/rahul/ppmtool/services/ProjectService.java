c18341306ec26b6549c6f21007dfc65303ea61b2
rahul3am <rahul720p@gmail.com>
2022-07-15 13:17:07 +0530

    custom exception (if we send same obj again)

--- ppmtool/src/main/java/com/rahul/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/rahul/ppmtool/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rahul.ppmtool.domain.Project;
+import com.rahul.ppmtool.exceptions.ProjectIdException;
 import com.rahul.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -15,9 +16,17 @@ public class ProjectService {
 	 public Project saveOrUpdateProject(Project project){
 
 	        //Logic
+		 	try {
+		 		 project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+		 		return projectRepository.save(project);
+		 	}
+		 	catch(Exception e) {
+		 		 throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+	        }
 
-	        return projectRepository.save(project);
+	        //Logic
+
+	        //return projectRepository.save(project);
 	    }
 
-	
-}
+	}
