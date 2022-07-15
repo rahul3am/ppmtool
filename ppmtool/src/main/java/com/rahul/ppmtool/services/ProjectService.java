6230b348c80d42d8b85ac10e61aa8ac3a32bce80
rahul3am <rahul720p@gmail.com>
2022-07-15 14:00:03 +0530

    find by identifier

--- ppmtool/src/main/java/com/rahul/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/rahul/ppmtool/services/ProjectService.java
@@ -28,5 +28,18 @@ public class ProjectService {
 
 	        //return projectRepository.save(project);
 	    }
+	 
+	 public Project findProjectByIdentifier(String projectId) {
+		 
+		 Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		 
+		 if(project == null) {
+			 throw new ProjectIdException("Project ID '"+projectId+"' does not exists");
+			 
+		 }
+		 
+		 return project;
+		 
+	 }
 
 	}
