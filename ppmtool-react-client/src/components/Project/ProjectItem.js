c49ca246d1a036887120456944f83f4f063281f8
rahul3am <rahul720p@gmail.com>
2022-07-16 15:54:41 +0530

    Project item and Header

---
+++ ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+ class ProjectItem extends Component {
+  render() {
+    return (
+      <div>ProjectItems</div>
+    )
+  }
+}
+
+export default ProjectItem;
